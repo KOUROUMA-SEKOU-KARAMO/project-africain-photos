630201b51b7db24710c2e139bec44cedf08519d9
Sekou Kourouma <sekoukourouma400@gmail.com>
2023-06-06 16:37:09 -0400

    La version 2 de la navbar 1

---
+++ index.js
@@ -0,0 +1,19 @@
+const bntCategories=document.querySelector('.container-navbar .btn-categorie')
+const showNavbar=document.querySelector('.container-navbar show-navbar')
+const categories=document.querySelector('.container-navbar .categories')
+const designs=document.querySelector('.container-navbar .designs')
+const photos=document.querySelector('.container-navbar .photos')
+const designsSubmenu=document.querySelector('.designs-submenu');
+
+const height="229px";
+
+// const showDesign='none';
+// bntCategories.addEventListener('mouseenter',(e)=>{
+// categories.classList.toggle('show-categories');
+// categories.style.setProperty('--height',height);
+// designsSubmenu.style.display='grid';
+// designsSubmenu.style.top=0;
+
+// })
+
+
